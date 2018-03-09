5dc5a1e1c03a4a4bb613940eee56149bf284b491
johnliu <john.liu@karmadata.com>
2018-03-09 16:52:42 -0500

    fixed cart updating mechanism

--- client/CartView.js
+++ client/CartView.js
@@ -3,18 +3,35 @@ const TopNavView = require('./TopNavView');
 const LeftNavView = require('./LeftNavView');
 const actions = require('./modelactions');
 
+async function getCart() {
+    let cart = actions.getCart();
+    let books = await m.request({
+        method: 'POST',
+        url: 'http://localhost:3570/listproducts',
+        data: { isbn: cart.map(item => item.isbn) },
+    });
+    cart.forEach(item => { item.book = books.find(b => b.isbn === item.isbn )});
+    return cart;
+}
+
+async function updateCart(srcCart) {
+    let cart = actions.getCart();
+    srcCart.forEach(srcItem => {
+        let item = cart.find(it => it.isbn === srcItem.isbn);
+        if (!item) cart.push({ isbn: srcItem.isbn, quantity: srcItem.quantity });
+        else item.quantity = srcItem.quantity;
+    });
+    cart = cart.filter(it => it.quantity > 0);
+    actions.setCart(cart);
+}
+
 const CartView = {
     oninit: async function(vnode) {
         // console.log(args, requestPath);
         try {
             // console.log(vnode);
-            this.cart = actions.getCart();
-            this.books = await m.request({
-                method: 'POST',
-                url: 'http://localhost:3570/listproducts',
-                data: { isbn: this.cart.map(item => item.isbn) }
-            });
-            this.cart.forEach(item => { item.book = this.books.find(b => b.isbn === item.isbn )});
+            this.cart = await getCart();
+            this.edited = false;
         } catch(e) {
             console.log(e);
         }        
@@ -28,7 +45,7 @@ const CartView = {
                     m('div', { class: 'col-md-3' },
                         m(LeftNavView)
                     ),
-                    this.books != null ?
+                    this.cart != null ?
                         m('div', { class: 'col-md-9' },
                             m('h1', this.category),
                             m('table', { class: 'shoppingcart' },
@@ -41,14 +58,21 @@ const CartView = {
                                 this.cart.map(item => m('tr',
                                     m('td', item.book.title),
                                     m('td', item.book.price),
-                                    m('td', item.quantity),
-                                    m('td', item.book.price * item.quantity)
+                                    m('td',
+                                        m('input', { type: 'text', size: 3, value: item.quantity,
+                                            oninput: m.withAttr('value', (value) => { this.cart.find(it => it.isbn === item.isbn).quantity = value; this.edited = true; })
+                                        })
+                                    ),
+                                    m('td', Math.round(item.book.price * item.quantity * 100) / 100)
                                 )),
                                 m('tr',
                                     m('td', ''),
                                     m('td', ''),
-                                    m('td', ''),
-                                    m('td', this.cart.reduce((sum, item) => sum + item.book.price * item.quantity, 0)),
+                                    m('td',
+                                        m('button', { class: 'btn btn-outline-success my-2 my-sm-0', type: 'button',
+                                            onclick: async () => { await updateCart(this.cart); this.cart = await getCart(); } }, 'Update')
+                                    ),
+                                    m('td', Math.round(this.cart.reduce((sum, item) => sum + item.book.price * item.quantity, 0) * 100) / 100),
                                 )
                             )
                         ) :
