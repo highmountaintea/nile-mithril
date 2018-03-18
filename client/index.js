610d6fd779bda44221bbadb5cc82b868c05e4f2b
jl818 <jl818@coolimage.com>
2018-03-18 02:09:58 -0400

    added Google Analytics

--- client/index.js
+++ client/index.js
@@ -5,12 +5,27 @@ const SearchView = require('./SearchView');
 const BookView = require('./BookView');
 const CartView = require('./CartView');
 const ProfileView = require('./ProfileView');
+const gtag = window.gtag;
 
 
 m.route(document.body, '/home', {
-    '/home': HomeView,
+    '/home': {
+        onmatch: function(args) {
+            gtag('config', 'UA-115934341-1', {
+                'page_title' : 'home',
+                'page_path': window.location.hash
+            });
+        },
+        render: function(vnode) {
+            return m(HomeView, { ...vnode.attrs });
+        }
+    },
     '/category/:category': {
         onmatch: function(args) {
+            gtag('config', 'UA-115934341-1', {
+                'page_title' : 'category view',
+                'page_path': window.location.hash
+            });
             this.key = Date.now();
         },
         render: function(vnode) {
@@ -20,6 +35,10 @@ m.route(document.body, '/home', {
     },
     '/search/:term': {
         onmatch: function(args) {
+            gtag('config', 'UA-115934341-1', {
+                'page_title' : 'search view',
+                'page_path': window.location.hash
+            });
             this.key = Date.now();
         },
         render: function(vnode) {
@@ -29,6 +48,10 @@ m.route(document.body, '/home', {
     },
     '/book/:isbn': {
         onmatch: function(args) {
+            gtag('config', 'UA-115934341-1', {
+                'page_title' : 'product view',
+                'page_path': window.location.hash
+            });
             this.key = Date.now();
         },
         render: function(vnode) {
@@ -36,9 +59,23 @@ m.route(document.body, '/home', {
             return m(BookView, {key: this.key, ...vnode.attrs});
         }
     },
-    '/cart': CartView,
+    '/cart': {
+        onmatch: function(args) {
+            gtag('config', 'UA-115934341-1', {
+                'page_title' : 'cart',
+                'page_path': window.location.hash
+            });
+        },
+        render: function(vnode) {
+            return m(CartView, { ...vnode.attrs });
+        }
+    },
     '/profile': {
         onmatch: function(args) {
+            gtag('config', 'UA-115934341-1', {
+                'page_title' : 'profile',
+                'page_path': window.location.hash
+            });
             this.key = Date.now();
         },
         render: function(vnode) {
